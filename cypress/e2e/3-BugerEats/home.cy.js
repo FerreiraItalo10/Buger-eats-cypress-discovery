a2bd83c736280e714645de2955827f2814a54e85
Italo Silva <ferreiraitalo10@gmail.com>
2022-08-27 14:35:27 -0300

    Treinamento Cypress

--- cypress/e2e/3-BugerEats/home.cy.js
+++ cypress/e2e/3-BugerEats/home.cy.js
@@ -1,7 +1,7 @@
 describe ('home page', ()=>{
 
     it('app deve estar online', ()=>{
-        cy.viewport(1440,900) // resolução das imagens, padrão é 1000 por 660
+        //cy.viewport(1440,900) // resolução das imagens, padrão é 1000 por 660
         cy.visit('/') // acessar página solicitada
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats') // confirma se tem o elemento na página
     }) // caso de teste
